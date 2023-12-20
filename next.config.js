5266401a76945bc04ce98d4a1dc09920818dfe3e
HassanElgallouchi <helgallouchi@gmail.com>
2023-12-20 08:00:35 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     domains: ["githubusercontent.com"],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-module.exports = async (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    /* config options here */
-  };
-  return nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
 };
+
+module.exports = nextConfig;
