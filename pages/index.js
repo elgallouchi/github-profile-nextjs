78834e3af1614251ee77738554c6bf294557a3f2
HassanElgallouchi <helgallouchi@gmail.com>
2024-01-09 22:34:37 +0100

    update home page

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,16 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 // import useApi from "../hooks/useApi";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  const { error, loading, following } = useSelector((state) => state.following);
+
+  const dispatch = useDispatch();
   // const [repos, getRepos, error] = useApi();
   // useEffect(() => {
   //   console.log(repos);
-  //   getRepos("hassanElgallouchi");
+  //   getFollowing("hassanElgallouchi");
   //   console.log(repos, error);
   // }, []);
   return (
@@ -18,7 +22,21 @@ export default function Home() {
         <meta name="description" content="See more github profile details" />
         <link rel="icon" href="/user.png" />
       </Head>
-      <main className={styles.main}>gh</main>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <h2 className="">Home</h2>
+          <div className={styles.home_top}>
+            <h4 className="">Updates</h4>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+              asperiores minus quae, necessitatibus, ipsum perferendis ullam
+              perspiciatis blanditiis, aperiam nesciunt ratione laudantium
+              sapiente atque reiciendis. Esse, assumenda repellendus!
+              Distinctio, ab.
+            </p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
